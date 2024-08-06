c608c1731a1e4a1e8ae23e9eb8e4da3885fbf563
vaahtopahvi <kermakahvi@hotmail.com>
2024-08-07 02:08:30 +0300

    include components and update layout.jsx

---
+++ app/page.jsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect } from "react";
+//components
+import Hero from "@/components/Hero";
+import Explore from "@/components/Explore";
+import About from "@/components/About";
+
+const Home = () => {
+  //add locomotive scroll
+  useEffect(() => {
+    const loadLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      new LocomotiveScroll();
+    };
+
+    loadLocomotiveScroll();
+  })
+  return (
+    <div className='h-full overflow-x-hidden'>
+      <Hero />
+      <Explore />
+      <About />
+      { /* temporary div */}
+      <div className="h-[4000px]"></div>
+      </div>
+  )
+};
+
+export default Home
